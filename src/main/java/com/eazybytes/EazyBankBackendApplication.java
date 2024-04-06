42a48474f81d83304a5fda760aff5a92fded9e48
ashoka92 <ashokaa1947@gmail.com>
2024-04-06 22:51:05 +0530

    delete unwanted file

--- src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -3,7 +3,6 @@ package com.eazybytes;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-
 @SpringBootApplication
 public class EazyBankBackendApplication {
 
